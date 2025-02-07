ee89c1151a7821df8d0393e093dfb22002ed40ed
PhongNV <ptvn2312@gmail.com>
2025-02-07 14:45:36 +0700

    first commit

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "tunvn-Unity6WebGL-1.0.2";
+const contentToCache = [
+    "Build/Unity6_WebBuild.loader.js",
+    "Build/Unity6_WebBuild.framework.js.unityweb",
+    "Build/Unity6_WebBuild.data.unityweb",
+    "Build/Unity6_WebBuild.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
